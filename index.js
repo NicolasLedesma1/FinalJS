a9867ed74d239174d61ac8ffee9ab7fdebdd45a3
NicolasLedesma1 <n.ledesma0194@gmail.com>
2023-09-18 17:28:35 -0300

    push

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 let nombre = prompt ("Ingrese nombre")
+let apellido = prompt ("ingrese apellido")
 let edad = parseInt(prompt ("ingrese su edad"));
+const persona = {
+    nombre : nombre,
+    apellido : apellido,
+    edad : edad
+}
+
+console.log(persona);
+
 const ingresaste = edad >= 18 && edad <=35;
 const ingresaste2 = edad >= 36;
 
@@ -9,7 +18,7 @@ if (edad <= 17 ){
     let monto = parseInt(prompt ("Cuanto dinero necesita sin el simbolo $ ni puntos"))
     let interes = 2.21
         if(ingresaste) {  
-            alert ("¡Felicidades! Tienes financiación hasta en 48 cuotas!");
+            alert ("¡Felicidades" + persona.nombre + "! Tienes financiación hasta en 48 cuotas!");
             for (let i = 2; i <= 48 ; i++ ){
                 let resultado = porcentaje (monto, interes, i);
                 console.log("si elige " + i + " cuotas seran de $" + resultado.toFixed(2));
