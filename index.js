81f4a141156f35586f404ee4f0aa9aab058f5dca
NicolasLedesma1 <n.ledesma0194@gmail.com>
2023-09-19 09:00:15 -0300

    push2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 let nombre = prompt ("Ingrese nombre")
 let apellido = prompt ("ingrese apellido")
 let edad = parseInt(prompt ("ingrese su edad"));
@@ -15,15 +17,15 @@ const ingresaste2 = edad >= 36;
 if (edad <= 17 ){
     alert (" no puede ingresar a la pagina tiene que ser mayor de 18");   
 }   else {
-    let monto = parseInt(prompt ("Cuanto dinero necesita sin el simbolo $ ni puntos"))
+    var monto = parseInt(prompt ("Cuanto dinero necesita sin el simbolo $ ni puntos"))
     let interes = 2.21
         if(ingresaste) {  
-            alert ("¡Felicidades" + persona.nombre + "! Tienes financiación hasta en 48 cuotas!");
+            alert ("¡Felicidades " + persona.nombre + "! Tienes financiación hasta en 48 cuotas!");
             for (let i = 2; i <= 48 ; i++ ){
                 let resultado = porcentaje (monto, interes, i);
-                console.log("si elige " + i + " cuotas seran de $" + resultado.toFixed(2));
+                console.log("si elige " + i + " cuotas seran de $" + resultado.toFixed(2))
             }
-            console.log(nombre +" aguardamos su respuesta");
+            console.log(persona.nombre +" aguardamos su respuesta");
     }   else if(ingresaste2){
             alert ("¡Felicidades! Tienes financiación hasta en 24 cuotas!");
             console.log("Hola " + nombre);
